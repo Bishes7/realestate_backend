fb387664b70fc47202416759460ba61249555701
Bishes Adhikari <specialbishes9@gmail.com>
2025-08-07 16:47:30 +1000

    created a test api route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { dbConnect } from "./src/config/dbConnect.js";
+import userRoutes from "./src/routes/userRoutes.js";
 
 const app = express();
 const PORT = 8000;
@@ -15,3 +16,11 @@ dbConnect()
     });
   })
   .catch((error) => console.log(error));
+
+// test the api
+app.get("/", (req, res) => {
+  res.send("Server is live");
+});
+
+// routes for user
+app.use("/api/user", userRoutes);
