1e7173c0593e762939d48fe79dfa2998b560c008
Bishes Adhikari <specialbishes9@gmail.com>
2025-09-22 00:42:37 +1000

    token functionalioty updated for production

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -9,7 +9,8 @@ const generateToken = (res, userId) => {
   // set jwt as HTTP-only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
